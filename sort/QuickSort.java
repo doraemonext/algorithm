acceb5054c5796b2cac69bb75e6482087f95e33a
doraemonext <doraemonext@gmail.com>
2022-04-11 23:46:35 +0800

    add quicksort template

---
+++ sort/QuickSort.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class QuickSort {
+
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 2, 4, 3};
+        shuffle(nums);
+        sort(nums, 0, nums.length - 1);
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+
+    private static void shuffle(int[] nums) {
+        Random random = new Random();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int tmp = nums[i];
+            int index = random.nextInt(i + 1);
+            nums[i] = nums[index];
+            nums[index] = tmp;
+        }
+    }
+
+    private static void sort(int[] nums, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(nums, lo, hi);
+        sort(nums, lo, j - 1);
+        sort(nums, j + 1, hi);
+    }
+
+    private static int partition(int[] nums, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        int value = nums[lo];
+        while (true) {
+            while (nums[++i] < value) {
+                if (i == hi) {
+                    break;
+                }
+            }
+            while (nums[--j] > value) {
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        swap(nums, lo, j);
+        return j;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
