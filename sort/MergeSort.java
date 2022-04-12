11f8218d076a769de7b259bd908845e85a3b3897
doraemonext <doraemonext@gmail.com>
2022-04-13 00:04:59 +0800

    add merge sort

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -1,2 +1,41 @@
 public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 4, 1, 3, 2};
+        sort(nums, 0, nums.length - 1);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    private static void sort(int[] nums, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(nums, lo, mid);
+        sort(nums, mid + 1, hi);
+        merge(nums, lo, mid, hi);
+    }
+
+    private static void merge(int[] nums, int lo, int mid, int hi) {
+        int[] aux = new int[nums.length];
+        int i = lo;
+        int j = mid + 1;
+
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = nums[k];
+        }
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                nums[k] = aux[j++];
+            } else if (j > hi) {
+                nums[k] = aux[i++];
+            } else if (aux[i] > aux[j]) {
+                nums[k] = aux[j++];
+            } else {
+                nums[k] = aux[i++];
+            }
+        }
+    }
 }
