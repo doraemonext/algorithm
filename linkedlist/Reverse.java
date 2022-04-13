ad59cf80d0cbf3620f59c85ca9816f8365443a7e
doraemonext <doraemonext@gmail.com>
2022-04-13 23:21:58 +0800

    add reverse linked-list

---
+++ linkedlist/Reverse.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+import common.ListNode;
+
+/**
+ * 反转链表
+ */
+public class Reverse {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
