46bd0fa4e8c56d4e813dd6570d7f5728ffe94d09
doraemonext <doraemonext@gmail.com>
2022-04-13 23:23:37 +0800

    add reverse linked-list

--- linkedlist/Reverse.java
+++ linkedlist/Reverse.java
@@ -7,7 +7,23 @@ import common.ListNode;
  */
 public class Reverse {
 
-    public ListNode reverseList(ListNode head) {
+    public static void main(String[] args) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        for (int i = 0; i < 5; i++) {
+            ListNode current = new ListNode();
+            current.val = i;
+            tail.next = current;
+            tail = tail.next;
+        }
+        ListNode head = reverseList(dummyHead.next);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    private static ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
